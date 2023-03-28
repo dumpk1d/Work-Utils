0cd331e950bc8689047fbb8518c4d1393857d824
dumpk1d <vozniakkacper@yahoo.com>
2023-03-29 02:47:51 +0300

    + download cfg

--- openWorkEnv.go
+++ openWorkEnv.go
@@ -4,14 +4,41 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"runtime"
 )
 
 const GOOS = runtime.GOOS
 
-func createLinuxCfg() {
+func downloadCfgFile() (err error) {
+	//Create
+	out, err := os.Create("wenv.json")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 
+	//Get
+	resp, err := http.Get("https://raw.githubusercontent.com/dumpk1d/Work-Utils/main/wenv.json")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	//Check
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Status: %s", resp.Status)
+	}
+
+	//Write
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
@@ -20,7 +47,7 @@ func main() {
 		if _, err := os.Stat("wenv.json"); err == nil {
 			fmt.Println("Ok")
 		} else if errors.Is(err, os.ErrNotExist) {
-			os.Create("wenv.json")
+			fmt.Println(downloadCfgFile())
 		}
 	case "windows":
 		fmt.Println("404 Error")
