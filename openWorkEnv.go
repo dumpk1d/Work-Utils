25a8643dcb84507339a03c493fba51b8ef40eac8
dumpk1d <vozniakkacper@yahoo.com>
2023-03-29 16:44:43 +0300

    after xbt review

--- openWorkEnv.go
+++ openWorkEnv.go
@@ -5,38 +5,48 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
-	"runtime"
 )
 
-// определяет ОС
-const cfgUrl string = "https://raw.githubusercontent.com/dumpk1d/Work-Utils/main/wenv.yml"
-const cfgFileName string = "wenv.yml"
+func envOrDefault(key, def string) string {
+	val, exists := os.LookupEnv(key)
+	if !exists {
+		return def
+	}
+
+	return val
+}
+
+var (
+	cfgUrl      = envOrDefault("CFG_URL", "https://raw.githubusercontent.com/dumpk1d/Work-Utils/main/wenv.yml")
+	cfgFileName = envOrDefault("CFG_FILE_NAME", "wenv.yml")
+)
 
 // Скачивает файл конфига из репозитория
 func downloadCfgFile() (err error) {
-	//Создание файла
+	// Создание файла
 	out, err := os.Create(cfgFileName)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
-	//Get
+	// Get
 	resp, err := http.Get(cfgUrl)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
-	//Проверка ответа
+	// Проверка ответа
 	if resp.StatusCode != http.StatusOK {
 		os.Remove(cfgFileName)
 		return fmt.Errorf("Status: %s", resp.Status)
 	}
 
-	//Запись
+	// Запись
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return err
@@ -48,23 +58,17 @@ func downloadCfgFile() (err error) {
 // initCfg() { //парсинг ? конфига ( тут начинается мясо))))) )
 
 func main() {
-
-	switch runtime.GOOS {
-	case "linux":
-		if _, err := os.Stat(cfgFileName); err == nil { // Если файл на месте
-			fmt.Println(string("\033[32m"), "Ok")
-			fmt.Println()
-		} else if errors.Is(err, os.ErrNotExist) { // Если файл не на месте
-			if err = downloadCfgFile(); err == nil {
-				fmt.Println(string("\033[32m"), "Done")
-			}
-			fmt.Println(string("\033[31m"), err)
+	_, err := os.Stat(cfgFileName)
+	if err != nil {
+		if !errors.Is(err, os.ErrNotExist) {
+			log.Fatalln(err)
 		}
 
-	case "windows":
-		fmt.Println("Nope:)")
-
-	default:
-		fmt.Println("Error stop 00000000")
+		err := downloadCfgFile()
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
+
+	fmt.Println(string("\033[32m"), "Ok")
 }
