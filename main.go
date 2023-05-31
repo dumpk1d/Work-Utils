c26865905cc93f9148f1e790294e6a3468a15930
dumpk1d <vozniakkacper@yahoo.com>
2023-05-31 22:33:11 +0300

    +GetBackupVmList test

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 const (
@@ -13,6 +17,18 @@ const (
 	warning  uint8 = 2
 	critical uint8 = 3
 )
+
+type logstruct struct {
+	Level    string    `json:"level"`
+	Func     string    `json:"func"`
+	Point    string    `json:"point"`
+	Task     string    `json:"task"`
+	Command  string    `json:"command"`
+	IsSystem bool      `json:"is_system"`
+	Vmid     int       `json:"vmid"`
+	Time     time.Time `json:"time"`
+	Message  string    `json:"message"`
+}
 
 func main() {
 
@@ -30,13 +46,13 @@ func main() {
 	fmt.Println("VM'S:", vms, "\n", "status", status)
 }
 
-func GetAllVmsList() (mass []string, status uint8) {
+func GetAllVmsList() (arg []string, status uint8) {
 
 	var cmd = "virsh -c qemu:///system list --all | grep one | awk '{print $2}'"
 	out, err := exec.Command("bash", "-c", cmd).Output()
 
 	if err != nil {
-		return []string{" "}, unknow
+		return []string{err.Error()}, unknow
 	} else {
 		var output = string(out)
 		if output == " " {
@@ -48,8 +64,18 @@ func GetAllVmsList() (mass []string, status uint8) {
 	}
 }
 
-func GetBackupVmList() {
-
+func GetBackupVmList() (arg []string, status uint8) {
+	jsonFile, err := os.Open("borg-agent.log")
+	if err != nil {
+		fmt.Println(err)
+		return []string{err.Error()}, unknow
+	} else {
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		var data logstruct
+		json.Unmarshal(byteValue, &data)
+		fmt.Println(data)
+		return []string{" "}, ok
+	}
 }
 
 func GetBaclkilstVmList() {
